ffa6850dcf7c3f198f267e904526793b3dd3f98f
wt <595517713@qq.com>
2021-02-05 15:46:29 +0800

    build(setup): 创建setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from distutils.core import setup
+
+with open("README.md", 'r', encoding='utf-8') as f:
+    long_desc = f.read()
+
+setup(
+    name="PyTools",
+    version="0.0.1",
+    description="Some Python Tools.",
+    long_description=long_desc,
+    author="QWERDF007",
+    author_email="595517713@qq.com",
+    url="https://github.com/QWERDF007/Pytools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
